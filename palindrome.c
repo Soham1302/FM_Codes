c17afb9b63c61ad9099f46219b95e94139d22dd7
Soham1302 <80255611+Soham1302@users.noreply.github.com>
2022-03-12 23:18:07 +0530

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -8,17 +8,16 @@ printf("Enter a string:");
 scanf("%s", string1);
 length = strlen(string1);
 for(i=0;i < length ;i++){
-
-if(string1[i] != string1[length-i-1]){
-flag = 1;
-break;
+  if(string1[i] != string1[length-i-1]){
+    flag = 1;
+    break;
+  }
 }
-}
-if (flag) {
-printf("%s is not a palindrome", string1);
+if (flag){
+  printf("%s is not a palindrome", string1);
 }
 else {
-printf("%s is a palindrome", string1);
+  printf("%s is a palindrome", string1);
 }
-return 0;
+  return 0;
 }
